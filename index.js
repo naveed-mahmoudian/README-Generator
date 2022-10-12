a3de58a3711698da282ab244cec4f35237f54efc
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-12 13:13:19 -0700

    Added initial questions

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message:
+      "Provide a short description explaining the what, why and how of your project.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for usage of your project.",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "List yourself and collaborators for credits.",
+  },
+];
+
+// Function to gather answers
+function getAnswers()
 
 // Function to write README file
 function writeToFile(fileName, data) {}
