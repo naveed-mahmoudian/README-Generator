5c8e3bbae50d52adfcbe68dfc135a4e0e05cef90
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-12 13:05:08 -0700

    Added starter code

---
+++ index.js
@@ -0,0 +1,15 @@
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Array of questions for user input
+const questions = [];
+
+// Function to write README file
+function writeToFile(fileName, data) {}
+
+// Initialization function
+function init() {}
+
+// Calling init function
+init();
