42c2bb8436f3bb8d64d47eb923f298cf686c49bd
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-12 15:20:31 -0700

    Added license verification and options

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const { default: test } = require("node:test");
 const questionsArr = [
   {
     type: "input",
+    name: "fullName",
+    message: "What is your full name?",
+  },
+  {
+    type: "input",
+    name: "year",
+    message: "What year did you create your project?",
+  },
+  {
+    type: "input",
     name: "title",
     message: "What is your project title?",
   },
@@ -55,7 +65,40 @@ const questionsArr = [
 
 // Function to gather answers
 function getAnswers(questions) {
-  inquirer.prompt(questions).then((answers) => writeToFile(answers));
+  inquirer.prompt(questions).then((answers) => {
+    if (answers.license === "Apache") {
+      answers.license = "Apache License covers XYZ";
+      writeToFile(answers);
+    } else if (answers.license === "GNU") {
+      answers.license = `Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+      Everyone is permitted to copy and distribute verbatim copies
+      of this license document, but changing it is not allowed.`;
+      writeToFile(answers);
+    } else if (answers.license === "MIT") {
+      answers.license = `MIT License
+
+      Copyright (c) ${answers.year} ${answers.fullName}
+      
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.`;
+      writeToFile(answers);
+    }
+  });
 }
 
 // Function to write README file
@@ -107,7 +150,7 @@ ${credits}
 ${tests}
 
 ## Questions
-GitHub Username: [${github}](https://www.github.com/${github}/)
+GitHub: [${github}](https://www.github.com/${github}/)
 
 Email: ${email}`,
     (error) => {
